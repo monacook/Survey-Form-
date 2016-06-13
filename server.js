7c73b9efed10024e1a507eeb075bc5b4be630949
monacook <cook.mona@gmail.com>
2016-06-12 21:23:35 -0700

    added more things onto server and dojoinfo

--- server.js
+++ server.js
@@ -5,23 +5,28 @@ var path = require("path");
 // create the express app
 var app = express();
 // static content
-app.use(express.static(path.join(__dirname, "./static/style")));
+app.use(express.static(path.join(__dirname, "./static")));
 // setting up ejs and our views folder
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 // root route to render the index.ejs view
-app.get('./views/main', function(req, res) {
-  res.render("main");
-})
 app.get('/', function(req, res) {
  res.render("index");
 })
 // post route for adding a user
 app.post('/users', function(req, res) {
- console.log("POST DATA", req.body);
+  var name = req.body.name;
+  var location = req.body.location;
+  var language = req.body.language;
+  var comment = req.body.comment;
+  var users = {name:name, location:location, language:language, comment:comment};
+
+ console.log("POST DATA \n\n", req.body);
+
  // This is where we would add the user to the database
  // Then redirect to the root route
- res.redirect('/views/main');
+ res.render('/dojoinfo', {users:users});
+
 })
 // tell the express app to listen on port 8000
 app.listen(8000, function() {
